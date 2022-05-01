cb6ca3bd7cb801bd3428cf42d0d16b9c116c7e57
Mathieu <mathieu.busse@live.fr>
2022-05-01 19:13:22 +0200

    Oké

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\QuestionType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/question', name: 'question_')]
+class QuestionController extends AbstractController
+{
+    #[Route('/ask', name: 'form')]
+    public function index(Request $request): Response
+    {
+
+        $formQuestion = $this->createForm(QuestionType::class);
+
+        $formQuestion->handleRequest($request);
+
+        if ($formQuestion->isSubmitted() && $formQuestion->isValid()) {
+        }
+
+        return $this->render('question/index.html.twig', [
+            'form' => $formQuestion->createView(),
+        ]);
+    }
+}
